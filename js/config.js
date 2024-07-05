64d05e633251b58bc6aade959d5f22f9eb3b8154
Nightsee <younes.naitsi.2002e@gmail.com>
2024-07-05 23:19:50 +0100

    updating cognito pool and client ids and api invoke url.

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_NNn9eqeRa', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '61cm7687ea78vj973esi3peem9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_GDowMFgLx', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '41m0bii62cuq1tv3leks89aqqe', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://s127rzfh42.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://rvciuguaml.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
