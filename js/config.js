66ca76744521fa54f18f07b5e18e6814e2330c93
YOUNES NAIT-SI <88140911+Nightsee@users.noreply.github.com>
2024-07-06 15:44:16 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_Qlud0B0pN', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '28gh9plh9sqthtet6fa7k50kr9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_8ylz4buy5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4c76a3ip912520hi9labqrqc5r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://j3xf7whts9.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://qyu59tt7l2.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
