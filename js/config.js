ca89c8f7ccccfcc75d263007fed795e40633200b
YOUNES NAIT-SI <88140911+Nightsee@users.noreply.github.com>
2024-07-06 17:17:08 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_6519MnZYz', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5qfo2pdtv6qeol41393u8uis5c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_4i6eZVfdH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4glnve35b7hul8emv735pv5s3i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://19uvv3dara.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://5jgzvxl115.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
